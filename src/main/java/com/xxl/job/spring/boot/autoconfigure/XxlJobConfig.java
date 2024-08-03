fb6de4ef76e27a28e451d3fbc6910782c3f6455f
hulk <wuhao@uisnx.com>
2024-08-03 22:37:11 +0800

    增加注解:ConditionalOnMissingBean 解决项目无法自定义配置类的问题

--- src/main/java/com/xxl/job/spring/boot/autoconfigure/XxlJobConfig.java
+++ src/main/java/com/xxl/job/spring/boot/autoconfigure/XxlJobConfig.java
@@ -4,6 +4,7 @@ import com.xxl.job.core.executor.impl.XxlJobSpringExecutor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -44,6 +45,7 @@ public class XxlJobConfig {
     }
 
     @Bean
+    @ConditionalOnMissingBean
     public XxlJobSpringExecutor xxlJobExecutor() {
         logger.info(">>>>>>>>>>> xxl-job config init.");
         XxlJobSpringExecutor xxlJobSpringExecutor = new XxlJobSpringExecutor();
